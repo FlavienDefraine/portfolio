a3bd8aa7b27b5f68b166be0945e5b9283f3ce597
FlavienDefraine <46069589+FlavienDefraine@users.noreply.github.com>
2023-12-28 21:50:33 +0100

    Add files via upload

---
+++ js/projects.js
@@ -0,0 +1,52 @@
+const projects = [
+	{
+		title: "Python Flask",
+		description: "Retrouvez mon exemple d'API sur les chiens :)",
+		imageUrl: "flask-api.png",
+		technologies: ["Python", "Restful API"],
+		links: [
+			"https://github.com/FlavienDefraine/API-Python-Flask",
+			"https://replit.com/@defflavien/APIFlaskFlavien#main.py",
+		],
+	},
+	{
+		title: "Landing Page",
+		description: "Retrouvez mon exemple",
+		imageUrl: "front-01.png",
+		technologies: ["Html", "Css"],
+		links: [
+			"https://github.com/FlavienDefraine/Landing-Page",
+			"https://flaviendefraine.github.io/Landing-Page/",
+		],
+	},
+	{
+		title: "Game Manager",
+		description: "lorem ipsum numero tres",
+		imageUrl: "game-manager.png",
+		technologies: ["Bootstrap 5", "Javascript"],
+		links: [
+			"https://github.com/web-rest-api/tp-1-bootstrap",
+			"https://web-rest-api.github.io/tp-1-bootstrap/",
+		],
+	},
+	{
+		title: "Favorite Books",
+		description: "lorem ipsum numero tres",
+		imageUrl: "favorite-books.png",
+		technologies: ["Bootstrap 5", "Javascript", "Resful API"],
+		links: [
+			"https://github.com/web-rest-api/tp3-bootstrap-api",
+			"https://web-rest-api.github.io/tp4-bootstrap-api/",
+		],
+	},
+	{
+		title: "Users",
+		description: "Uthentication API",
+		imageUrl: "nodeJs-express.png",
+		technologies: ["NodeJs", "Express"],
+		links: [
+			"https://replit.com/@MartinPedraza/node-expess#index.js",
+			"https://node-expess.martinpedraza.repl.co/",
+		],
+	},
+]
